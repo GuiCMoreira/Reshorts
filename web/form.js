09c3d2673693b2485ed1172e4c509d469f47f0e9
Guilherme Carvalho <gui0307carvalho@gmail.com>
2023-09-17 18:42:18 -0300

    Update

--- web/form.js
+++ web/form.js
@@ -1,21 +1,24 @@
+import { server } from "./server.js"
+
 const form = document.querySelector("#form")
 const input = document.querySelector("#url")
 const content = document.querySelector("#content")
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault()
 
   const videourl = input.value
 
   if (!videourl.includes("shorts")) {
-    return content.textContent = "Este vídeo não parece ser um short"
-  } 
+    return (content.textContent = "Este vídeo não parece ser um short")
+  }
 
   const [_, params] = videourl.split("/shorts/")
   const [videoID] = params.split("?si")
-  console.log(videoID)
 
   content.textContent = "Obtendo o texto do áudio"
 
-  
+  const transcription = await server.get("/summary/" + videoID)
+
+  content.textContent = transcription.data.result
 })
